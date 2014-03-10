574d05dad03e8af0076011b2844301174e1f6976
A. Prooks <aprooks@live.ru>
2014-03-10 16:38:21 +0300

    test: e2e basic test for problems
    Signed-off-by: A. Prooks <aprooks@live.ru>

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -25,13 +25,13 @@ angular.module('myApp', ['app.config', 'ngRoute', 'ngAnimate', 'ngResource',
         controller: 'ProblemEditCtrl',
         templateUrl: TPL_PATH + '/problemEdit.html'
       })
-      .when('/videos/:id', {
-        controller: 'VideoCtrl',
-        templateUrl: TPL_PATH + '/video.html'
-      })
       .when('/videos/create',{
         controller:'CreateVideoCtrl',
         templateUrl: TPL_PATH + '/createVideo.html'
+      })
+      .when('/videos/:id', {
+        controller: 'VideoCtrl',
+        templateUrl: TPL_PATH + '/video.html'
       })
     ;
   });
