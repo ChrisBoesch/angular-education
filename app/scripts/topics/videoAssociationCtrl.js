5732570243d1e9f436b0bc1e97e1286e30fcc17c
A. Prooks <aprooks@live.ru>
2014-04-09 23:54:54 +0300

    feat: video can be added to topic
    preview, controller not finished and no tests
    
    Signed-off-by: A. Prooks <aprooks@live.ru>

--- app/scripts/topics/videoAssociationCtrl.js
+++ app/scripts/topics/videoAssociationCtrl.js
@@ -1,16 +1,19 @@
 angular
 .module('app.topics')
 .controller('VideoAssociationCtrl',function($scope,videos){
+  //TODO: should not show already added
   videos.all().then(function(data){
     $scope.list = data;
   });
 
+  //todo: add back to available videos
   $scope.removeVideo = function(topic,video){
     topic.videos = topic.videos.filter(function(vid){
-      debugger;
-      return vid.id!=video.id;
+      return vid.id!==video.id;
     });
   };
+  
+  //todo: remove added from list
   $scope.addVideo = function(topic,video){
     topic.videos.push(video);
   };
